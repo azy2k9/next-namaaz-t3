8a1a61a9cc4e1b14ec55e18ec7c1fc591bdd9ffd
Arslaan Qadus <azy2k9@gmail.com>
2022-09-07 20:10:19 +0100

    update ts

--- types.d.ts
+++ types.d.ts
@@ -13,3 +13,16 @@ type Timetable = Namaaz[];
 type Time = `${number}.${number}${number} ${'am' | 'pm'}`;
 
 type Meridiem = 'am' | 'pm';
+
+type NamaazTimes = {
+  current: {
+    name: string;
+    time: Date;
+    difference: number;
+  };
+  next: {
+    name: string;
+    time: Date;
+    difference: number;
+  };
+};
