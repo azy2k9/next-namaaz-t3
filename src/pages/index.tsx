4a52ddaeab49cd7582b8b186d24676abb23eb489
Arslaan Qadus <azy2k9@gmail.com>
2022-09-10 15:17:16 +0100

    Bugfix: Center the namaaz times

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,11 +16,11 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="container flex flex-col min-h-screen min-w-full">
-        <section className="flex flex-col justify-center items-center flex-1 bg-green-600 text-white font-semibold text-9xl">
+        <section className="flex flex-col justify-center items-center flex-1 bg-green-600 text-white font-semibold text-9xl text-center">
           <span className="text-5xl pb-2">{current.name}</span>
           <span>{current.time.format('hh:mm a')}</span>
         </section>
-        <section className="flex flex-col justify-center items-center flex-1 bg-green-800 text-white font-semibold text-9xl">
+        <section className="flex flex-col justify-center items-center flex-1 bg-green-800 text-white font-semibold text-9xl text-center">
           <span className="text-5xl pb-2">{next.name}</span>
           <span>{next.time.format('hh:mm a')}</span>
         </section>
